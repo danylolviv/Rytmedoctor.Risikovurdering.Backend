e453a24a370ced4b18db570355212160a44b411d
Danylo <danyloboshagora@gmail.com>
2021-11-22 08:44:24 +0100

    uploading backend solution with core and domain projects inside of it

---
+++ DanyloSoft.Rytmedoktor.Risikovurdering.Form.Backend.Core.Test/Models.Test/FormAnswerOptionTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace DanyloSoft.Rytmedoktor.Risikovurdering.Form.Backend.Core.Test.Models
+{
+  public class FormAnswerOptionTest
+  {
+    [Fact]
+    public void AnswerOption_IsAvailable()
+    {
+      var option = new FormAnswerOption();
+    }
+  }
+
+  public class FormAnswerOption
+  {
+    
+  }
+}
---
+++ DanyloSoft.Rytmedoktor.Risikovurdering.Form.Backend.Core.Test/Models.Test/FormQuestionTest.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace DanyloSoft.Rytmedoktor.Risikovurdering.Form.Backend.Core.Test.Models
+{
+  public class FormQuestionTest
+  {
+    
+  }
+}
