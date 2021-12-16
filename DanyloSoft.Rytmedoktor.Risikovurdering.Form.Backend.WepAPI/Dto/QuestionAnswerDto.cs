89799f3f6f25d7e7ba84c005fb52d5960fbcfaef
Danylo <danyloboshagora@gmail.com>
2021-12-16 02:46:14 +0100

    naming conflict

---
+++ DanyloSoft.Rytmedoktor.Risikovurdering.Form.Backend.WepAPI/Dto/QuestionAnswerDto.cs
@@ -0,0 +1,8 @@
+namespace DanyloSoft.Rytmedoktor.Risikovurdering.Form.Backend.WepAPI.Dto
+{
+  public class QuestionAnswerDto
+  {
+    public string question { get; set; }
+    public string answer { get; set; }
+  }
+}
